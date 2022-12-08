9c1ae8be4c3d7239220b6ec2c00fa8368f02c7a9
MusicalArtist12 <Themusicalartist12@gmail.com>
2022-12-08 13:17:57 -0800

    beautiful code

--- Day-1/d1-p1.cpp
+++ Day-1/d1-p1.cpp
@@ -1,51 +1,48 @@
-// number of calories each elf is carrying 
-// elves take turns writing down the number of calories contained in each meal
-// They need to know which elf to ask if they run out
-
 #include <iostream>
 #include <fstream>
 #include <vector>
 
-int addElf(std::fstream& fin);
-bool includes(int arr[], int n, int check);
+int addElf(std::fstream& fin); // gets values inbetween spaces, also avoids nested while loops
+bool includes(int arr[], int n, int check); // int array function to check if a int is in the int array
 
 int main() {
+    // Load vector of elves from file
     std::fstream fin;
-    fin.open("puzzle-input.txt", std::ios::in);
-
     std::vector <int> elves;
-
     std::string line;
-    int linenum = 1;
 
+    fin.open("puzzle-input.txt", std::ios::in);
     while(!fin.eof()) {
-
         elves.push_back(addElf(fin));
 
-        linenum++;
     } fin.close();
-
-
     
+    // declare and initialize an array for the top 3
     int n = 3;
-    int top[] = {-1, -1, -1};  
+    int top[n];  
+    for(int i = 0; i < n; i++) top[i] = -1; // -1 avoids bugs and acts as an int NULL
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; (j < elves.size()); j++) {
-            if(
-                elves[j] > elves[top[i]] &&
-                !includes(top, n, j)
-            ) top[i] = j;
+    // get the top 3 from the vector
+    for(int i = 0; i < n; i++) { // do this n times
+        for(int j = 0; (j < elves.size()); j++) { // check against all elves
+            if( elves[j] > elves[top[i]] && !includes(top, n, j)) {
+                top[i] = j;
+            }
         }
-    }
+    }/*  
+        This automatically sorts the list too; 
+        we exclude the last one the next time around
+    */
 
+    // Get the sum of the top 3
     int sum = 0;
-
     for(int i = 0; i < n; i++) {
+        std::cout << "elf(" << top[i] << ") = " << elves[top[i]] << std::endl;
         sum += elves[top[i]];
     }
-    std::cout << "top: elf(" << top[0] << ") = " << elves[top[0]] << std::endl;
-    std::cout << "sum of top 3: " << sum << std::endl;
+
+    std::cout << "\nsum of top 3: " << sum << std::endl;
+
     return 0;
 }
 
@@ -53,13 +50,13 @@ int addElf(std::fstream& fin) {
     std::string line;
     int cal = 0;
 
-    getline(fin, line, '\n');
+    getline(fin, line, '\n'); // initialize line
 
-    while(!line.empty()) {
-        cal += stoi(line);
+    while(!line.empty()) { 
+        cal += stoi(line); // get the sum between spaces
         getline(fin, line, '\n');
 
-        if(fin.eof()) break;
+        if(fin.eof()) break; // check since the last line isn't "empty"
     }
 
     return cal;
